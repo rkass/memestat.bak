a7a8776a7704eb4da3c141babff98b9f76642f05
rkass <rck72@cornell.edu>
2012-11-24 15:48:06 -0500

    first

---
+++ reddit.py
@@ -0,0 +1,11 @@
+import urllib3
+import json
+
+adviceAnimals = urllib3.PoolManager().request('GET',
+  'http://reddit.com/r/adviceanimals.json')
+aajson = json.loads(adviceAnimals.data)
+thumbnail = aajson['data']['children'][0]['data']['thumbnail']
+thumbnailPage = urllib3.PoolManager().request('GET', thumbnail)
+f = open('test.jpg', 'wb')
+f.write(thumbnailPage.data)
+f.close()
