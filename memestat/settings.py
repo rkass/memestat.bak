c55605c66a83b62c2e6a3ec7f4325b29f9a2eb12
rkass <rck72@cornell.edu>
2012-11-26 18:48:53 -0500

    templates now

--- memestat/settings.py
+++ memestat/settings.py
@@ -1,4 +1,5 @@
 # Django settings for memestat project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -27,7 +28,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -103,9 +104,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'memestat.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+  os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
--- memestat/views.py
+++ memestat/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def hello(request):
-  return HttpResponse("Hello World!")
+  return render_to_response('hello', {'location': 'World'})
